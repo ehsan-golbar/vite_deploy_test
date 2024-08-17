f5e35da6c0563730b6b67f1a92fb0d18fb5994f1
Ehsan <1382ehg@gmail.com>
2024-08-17 14:03:50 +0330

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite_deploy_test.git/"
+  base: "/vite_deploy_test/"
 })
